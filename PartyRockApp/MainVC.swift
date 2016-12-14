265e17592269519a3e09fbeabd1628d76e9045c2
Grant Rosario <grantster1124@aol.com>
2016-12-13 23:50:02 -0500

    Initial commit - groups made, basic UI done

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Grant Rosario on 12/13/16.
+//  Copyright © 2016 Grant Rosario. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ PartyRockApp/VideoVC.swift
@@ -0,0 +1,35 @@
+//
+//  VideoVC.swift
+//  PartyRockApp
+//
+//  Created by Grant Rosario on 12/13/16.
+//  Copyright © 2016 Grant Rosario. All rights reserved.
+//
+
+import UIKit
+
+class VideoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
